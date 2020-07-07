2327e5decfbc58052c67e15de57196e9c770e120
Alexander Pavlenko <pava@bk.ru>
2020-07-07 20:48:39 +0700

    Test created

--- README.md
+++ README.md
@@ -3,8 +3,8 @@
 ## Краткое описание
 
 1. Cоздан gradle проект
-2. Создан тестовый класс: 
-     - в поле телефонного номера вводится неправильное значение 
+2. Создан тестовый класс.
+3. Создан метод для получение требуемой даты. 
 3. Настроен CI
 
-## Project 
+## Project on Appveyor [![Build status](https://ci.appveyor.com/api/projects/status/octucp75dox5o3x7/branch/master?svg=true)](https://ci.appveyor.com/project/pava-14/aqa2-2-1/branch/master) 
--- src/test/java/ru/netology/carddelivery/CardDeliveryFormTest.java
+++ src/test/java/ru/netology/carddelivery/CardDeliveryFormTest.java
@@ -2,13 +2,10 @@ package ru.netology.carddelivery;
 
 import com.codeborne.selenide.SelenideElement;
 import org.junit.jupiter.api.Test;
-import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 
-import java.text.SimpleDateFormat;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
-import java.util.Calendar;
 
 import static com.codeborne.selenide.Condition.*;
 import static com.codeborne.selenide.Selectors.byText;
@@ -27,25 +24,19 @@ public class CardDeliveryFormTest {
     public void shouldCreditCardDeliveryOrder() {
 
         open("http://localhost:9999");
-        SelenideElement form = $("form");
-        form.$("[data-test-id=city] input").setValue("Новосибирск");
-        form.$("[data-test-id=date] input").doubleClick();
-        form.$("[data-test-id=date] input").sendKeys(Keys.BACK_SPACE);
-        form.$("[data-test-id=date] input").setValue(GetOrderDate(daysDiff));
-        form.$("[data-test-id=name] input").setValue("Иванов Петр Петрович");
-        form.$("[data-test-id=phone] input").setValue("+79099099090");
-        form.$("[data-test-id=agreement]").click();
-//        form.$("button[type=button]").click();
-        form.$(".grid-col .form-field.form-field_size_m").click();
-//        form.$(".button__content .spin").should(disappear);
-        $(withText("Успешно!")).shouldBe(visible);
-        $(byText("Встреча успешно запланирована на "
-                + GetOrderDate(daysDiff))).shouldBe(visible);
-//        form.$("[data-test-id=notification] .notification__title").should(appears);
-//        form.$("[data-test-id=notification] .notification__title").shouldHave(text("Успешно!"));
 
-        $(".notification .notification__content")
-                .shouldHave(exactText("Встреча успешно запланирована на "
-                         + GetOrderDate(daysDiff)));
+        SelenideElement element = $("form");
+        element.$("[data-test-id=city] input").setValue("Новосибирск");
+        element.$("[data-test-id=date] input").doubleClick();
+        element.$("[data-test-id=date] input").sendKeys(Keys.BACK_SPACE);
+        element.$("[data-test-id=date] input").setValue(GetOrderDate(daysDiff));
+        element.$("[data-test-id=name] input").setValue("Иванов Петр Петрович");
+        element.$("[data-test-id=phone] input").setValue("+79099099090");
+        element.$("[data-test-id=agreement]").click();
+        element.$$("button").find(exactText("Забронировать")).click();
+
+        $(withText("Успешно!")).waitUntil(visible, 15000);
+        $(byText("Встреча успешно забронирована на")).waitUntil(visible, 15000);
+        $(byText(GetOrderDate(daysDiff))).waitUntil(visible, 15000);
     }
 }
