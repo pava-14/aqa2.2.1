1ffe2ed155b6b2c009d1f0aa097ed7011e3c9e9b
Alexander Pavlenko <pava@bk.ru>
2020-07-09 21:42:58 +0700

    Teacher's comment #1 #2

--- src/test/java/ru/netology/carddelivery/CardDeliveryFormTest.java
+++ src/test/java/ru/netology/carddelivery/CardDeliveryFormTest.java
@@ -15,11 +15,11 @@ import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
 public class CardDeliveryFormTest {
-    private int daysDiff = 4;
+    private int daysOffset = 4;
 
-    private String GetOrderDate(int daysDiff) {
+    private String getOrderDate(int daysOffset) {
         DateTimeFormatter dateFormat = DateTimeFormatter.ofPattern("dd.MM.yyyy");
-        return dateFormat.format(LocalDateTime.now().plusDays(daysDiff));
+        return dateFormat.format(LocalDateTime.now().plusDays(daysOffset));
     }
 
     @Test
@@ -27,12 +27,13 @@ public class CardDeliveryFormTest {
 
         open("http://localhost:9999");
 
-        String dateOrder = GetOrderDate(daysDiff);
+        String dateOrder = getOrderDate(daysOffset);
         SelenideElement element = $("form");
+        SelenideElement fieldDate = element.$("[data-test-id=date] input");
         element.$("[data-test-id=city] input").setValue("Новосибирск");
-        element.$("[data-test-id=date] input").doubleClick();
-        element.$("[data-test-id=date] input").sendKeys(Keys.BACK_SPACE);
-        element.$("[data-test-id=date] input").setValue(dateOrder);
+        fieldDate.doubleClick();
+        fieldDate.sendKeys(Keys.BACK_SPACE);
+        fieldDate.setValue(dateOrder);
         element.$("[data-test-id=name] input").setValue("Иванов Петр Петрович");
         element.$("[data-test-id=phone] input").setValue("+79099099090");
         element.$("[data-test-id=agreement]").click();
